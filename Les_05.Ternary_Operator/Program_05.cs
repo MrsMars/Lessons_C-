4d161af0f387a5f8173e8787c9bb4964973e4ca2
Allega <mr.oger@mail.ru>
2018-08-24 16:07:32 +0300

    Ternary operator

---
+++ Les_05.Ternary_Operator/Program_05.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ternary operator
+namespace Les_05
+{
+    class Program_05
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ternary operator";
+
+            int a;
+            bool b;
+
+            Console.WriteLine("Enter a number");
+            a = int.Parse(Console.ReadLine());
+
+            b = false;
+            if(a % 3 == 0)
+            {
+                b = true;
+            }
+
+            Console.WriteLine(b ? (a + " is multiply to 3") : (a + " is't multiply to 3"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
