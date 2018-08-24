80a87aa625ba6066198d53e35496de73ce45a6a6
Allega <mr.oger@mail.ru>
2018-08-24 16:06:07 +0300

    Conditions

---
+++ Les_04.Conditions/Program_04.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Conditions - Условия if/else
+namespace Les_04
+{
+    class Program_04
+    {
+        static void Main(string[] args)
+        {
+            double a;
+
+            a = double.Parse(Console.ReadLine());       // you have to enter ',' like '5,8', because using '5.8' will caose of exception
+
+            if(a > 0)
+            {
+                Console.WriteLine(a + " > 0");
+            }
+            else if(a == 0)
+            {
+                Console.WriteLine(a + " = 0");
+            }
+            else
+            {
+                Console.WriteLine(a + " < 0");
+            }
+        }
+    }
+}
