95d8640f259bf4461641b1d3fc80c90a9502f260
Allega <mr.oger@mail.ru>
2018-08-24 16:02:47 +0300

    add 2nd lesson
    Arithmetic operations and parse

---
+++ Les_02/Program_02.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Arithmetic operations - Арифметические действия
+namespace Les_02
+{
+    class Program_02
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+
+            Console.WriteLine("Enter a: ");
+            a = double.Parse(Console.ReadLine());          // Parse - transformation at selected type (преобразование в указанный тип)
+
+            Console.WriteLine("Enter b:");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nArithmetic operations: ");
+            Console.WriteLine("Amount: " + (a + b));
+            Console.WriteLine("Difference: " + (a - b));
+            Console.WriteLine("Multiply: " + (a * b));
+            Console.WriteLine("Division: " + (a / b));
+
+            Console.ReadKey();
+        }
+    }
+}
