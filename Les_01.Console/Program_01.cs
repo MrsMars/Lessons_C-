57c807ef41bd5360f9de884ddd1925cd969e601c
Allega <mr.oger@mail.ru>
2018-08-24 16:01:05 +0300

    add lesson 1
    work with console(out)

---
+++ Les_01.Console/Program_01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Les_01
+{
+    class Program_01
+    {
+        static void Main(string[] args)         // static - ключевое слово, показывающее что это независимая функция
+        {
+            Console.Write("Helo World");
+            Console.Write("Helo World");
+            Console.WriteLine("Helo World");
+            Console.WriteLine("Helo World");    // new string
+
+            Console.ReadKey();
+        }
+    }
+}
