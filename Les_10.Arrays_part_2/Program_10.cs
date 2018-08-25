826988bcb797e19ca0731928471ce5e9ec3c7e6b
Allega <mr.oger@mail.ru>
2018-08-25 19:53:31 +0300

    Arrays.part2
    2 and more D arrays, circle 'foreach'

---
+++ Les_10.Arrays_part_2/Program_10.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Circle 'foreach'
+namespace Les_10
+{
+    class Program_10
+    {
+        static void Main(string[] args)
+        {
+            int[] a1 = new int[5];
+            int[] a2 = new int[] { 343, 343, 056, 92, 9323 };
+            int[] a3 = new int[5] { 1, 45, 94, 9, 5 };
+
+            double[] d = new double[] { 2.8, 7.9 };
+
+            string[] s = new string[] { "pool", "red", "men" };
+
+            char[] c = new char[5] { '6', '_', '*', 'g', 'd' };
+
+            // 'foreach is only for reading
+            foreach(int i in a2)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("/n");
+
+            foreach (string str in s)
+            {
+                Console.Write(str + " ");
+            }
+        }
+    }
+}
