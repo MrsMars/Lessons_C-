6b6162967825508e2cf3b229818036a7dd86d76c
Allega <mr.oger@mail.ru>
2018-08-25 19:48:15 +0300

    Switch
    circle switch and label goto

---
+++ Les_07.Switch/Program_07.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// operator 'switch' and 'goto'
+namespace Les_07
+{
+    class Program_07
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Calculator";
+
+            double a, b;
+            char c;
+
+            l1:
+
+            Console.WriteLine("Enter a: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a sign: ");
+            c = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter b: ");
+            b = double.Parse(Console.ReadLine());
+        
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine("Amount: " + (a + b) + '\n');
+                    goto l1;
+                case '-':
+                    Console.WriteLine("Difference: " + (a - b) + '\n');
+                    goto l1;
+                case '*':
+                    Console.WriteLine("Muliply: " + (a * b) + '\n');
+                    goto l1;
+                case '/':
+                    Console.WriteLine("Division: " + (a / b) + '\n');
+                    goto l1;
+                default:
+                    Console.WriteLine("Error" + '\n');
+                    goto l1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
