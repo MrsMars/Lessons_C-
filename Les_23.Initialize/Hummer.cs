2c769ec46b9acc90eb7aa1eb32edb3ca2cf74385
Allega <mr.oger@mail.ru>
2018-08-27 13:24:07 +0300

    intializers

---
+++ Les_23.Initialize/Hummer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_23
+{
+    class Hummer
+    {
+        public string firm = null;
+        public string Material { get; set; }
+        public int Weight { get; set; }
+        public int price;
+
+
+        public Hummer() { }
+
+        public Hummer(int price)
+        {
+            this.price = price;
+        }
+    }
+}
---
+++ Les_23.Initialize/Program_23.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Инициализаторы классов - позволяют присвоить значения всем доступным полям и свойствам объекта без вызова конструктора
+
+namespace Les_23
+{
+    class Program_23
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer(2000)        // '(2000)' - with constructor
+            {
+                firm = "SomeFirm",
+                Material = "tree",
+                Weight = 2
+                //,price = 2000               - without constructor 
+            };
+
+            Console.WriteLine(hummer.firm + ", " + hummer.Material + ", " + hummer.Weight + ", " + hummer.price);
+
+
+        }
+    }
+}
