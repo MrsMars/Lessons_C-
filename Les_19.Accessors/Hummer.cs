7e54e1bd0b6e5c88ee69516c68e1d654d55874fa
Allega <mr.oger@mail.ru>
2018-08-27 13:20:25 +0300

    Get and set
    mutator and accessor

---
+++ Les_19.Accessors/Hummer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_19
+{
+    class Hummer
+    {
+        string material = null;
+
+        public string GetField()        // getter
+        {
+            return material;
+        }
+
+        public bool SetField(string material)   // setter
+        {
+            if (material.ToLower() != "metall")
+            {
+                this.material = material;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Les_19.Accessors/Program_19.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Методы доступа - позволяют получать доступ к закрытым полм класса
+//  1. Мутаторы(сеттеры)
+//  2. Аксессоры(геттеры)
+// аналогом этого являются свойства set and get
+
+namespace Les_19
+{
+    class Program_19
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer();
+            
+            string mat = Console.ReadLine();
+
+            if (hummer.SetField(mat))
+            {
+                Console.WriteLine(hummer.GetField());
+            }
+            else
+            {
+                Console.WriteLine(mat + "- Invalid value");
+            }
+        }
+    }
+}
