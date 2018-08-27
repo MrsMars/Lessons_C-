1baf98e4758d95628849b1e8d02370c2d765144f
Allega <mr.oger@mail.ru>
2018-08-27 13:25:14 +0300

    partial classes

---
+++ Les_24.Partial_Classes/Program_24.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Частичные классы - обычный класс, но разбитый на несколько частей
+
+namespace Les_24
+{
+    class Program_24
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer();
+            hummer.GetFirm();
+
+        }
+    }
+}
