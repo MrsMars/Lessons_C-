2dba1b60f98d64c2dd56d31f86cabb2c940a9cb2
Allega <mr.oger@mail.ru>
2018-08-27 13:23:24 +0300

    auto propertyes

---
+++ Les_22.Auto_Propertyes/Hummer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_22
+{
+    class Hummer
+    {
+        public string Firm { get; set; }
+        public string Material { get; set; }
+    }
+}
---
+++ Les_22.Auto_Propertyes/Program_22.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Автоматические свойства - используются, если нам не нужна дополнительна логика и нас устроит укороченная форма
+
+namespace Les_22
+{
+    class Program_22
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer();
+            hummer.Firm = "SomeFirm";
+            hummer.Material = "tree";
+
+            Console.WriteLine(hummer.Firm + ", " + hummer.Material);
+        }
+    }
+}
