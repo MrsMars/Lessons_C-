253090ad4ea404302054f665e41aaa3be40c241b
Allega <mr.oger@mail.ru>
2018-08-27 13:21:32 +0300

    Get & set

---
+++ Les_20.Propertyes_Get_and_Set/Hummer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_20
+{
+    class Hummer
+    {
+        string material = null;
+
+        public string Material
+        {
+            get
+            {
+                return material;
+            }
+            set
+            {
+                if (value.ToLower() != "metall")
+                {
+                    material = value;
+                }
+            }
+        }
+    }
+}
---
+++ Les_20.Propertyes_Get_and_Set/Program_20.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Propertyes get and set
+
+namespace Les_20
+{
+    class Program_20
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer();
+            hummer.Material = Console.ReadLine();
+
+            Console.WriteLine(hummer.Material);     // tree or ''
+        }
+    }
+}
