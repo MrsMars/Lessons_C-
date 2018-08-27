e8c11a4a1d70f0aa9fe5a08d48d0a8f18da2c47f
Allega <mr.oger@mail.ru>
2018-08-27 13:22:49 +0300

    Constructors of classes

---
+++ Les_21.Class_Constructors/Hummer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_21
+{
+    class Hummer
+    {
+        string firm = null;
+
+        public string Firm
+        {
+            get
+            {
+                return firm;
+            }
+            set
+            {
+                firm = value;
+            }
+        }
+
+        public string material = null;
+        public int weight;
+        public int price;
+
+        #region Constructors
+        // при создании пользовательского конструктора, нужно обязательно задать конструктор по умолчанию 
+
+        // Default constructor
+        public Hummer() { }
+
+        // User's constructor 1
+        public Hummer(string firm)
+        {
+            Firm = firm;
+        }
+
+        // User's constructor 2
+        public Hummer(string firm, string material, int weight)
+        {
+            Firm = firm;
+            this.material = material;
+            this.weight = weight;
+        }
+
+        // User's constructor 3
+        public Hummer(int price) : this("SomeFirm", "SomeMaterial", 2) // программа подберёт конструктор с подходящими входными параметрами в 'this'
+        {
+            this.price = price;
+
+        }
+        
+
+
+        #endregion
+    }
+}
---
+++ Les_21.Class_Constructors/Program_21.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Конструкторы класса
+
+namespace Les_21
+{
+    class Program_21
+    {
+        static void Main(string[] args)
+        {
+            Hummer hummer = new Hummer(2000);
+            Console.WriteLine("Firm {0}, material {1}, price {2}, weiht {3}", hummer.Firm, hummer.material, hummer.price, hummer.weight);
+            
+        }
+    }
+}
