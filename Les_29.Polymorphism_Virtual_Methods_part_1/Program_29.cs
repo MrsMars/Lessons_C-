43cda45c17199c1a2264a0bb61a0d1ed723d7fbd
Allega <42183540+MrsMars@users.noreply.github.com>
2018-08-28 21:12:16 +0300

    add virtual methods

---
+++ Les_29.Polymorphism_Virtual_Methods_part_1/Program_29.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Полимрфизм (виртуальные члены)
+
+namespace Les_29
+{
+    class Program_29
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.Method();              // Method from derived class
+        }
+    }
+}
