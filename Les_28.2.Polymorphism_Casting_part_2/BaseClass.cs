8dfbc839298411a8eec1be706e2c5e5d946bbb49
Allega <42183540+MrsMars@users.noreply.github.com>
2018-08-28 20:59:08 +0300

    add polymorphism part 2
    upcasting, strong and light ref

---
+++ Les_28.2.Polymorphism_Casting_part_2/BaseClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_28
+{
+    class BaseClass
+    {
+        public string jaket = "costum";
+    }
+}
---
+++ Les_28.2.Polymorphism_Casting_part_2/Program_28.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_28._2
+{
+    class Program_28
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+            Console.WriteLine(derivedClass.GetHashCode());          // 46104728
+
+            Console.WriteLine("------------------------- UpCast производного класса к базовому -----------------------------");
+
+            Console.WriteLine("------------------------- по сильной ссылке -----------------------------");
+
+            BaseClass derivedClassUp1 = derivedClass;         // or BaseClass baseClass = (BaseClass)derivedClass; 
+            Console.WriteLine(derivedClassUp1.GetHashCode());       // 46104728
+
+            Console.WriteLine("------------------------- по слабой ссылке -----------------------------");
+
+            BaseClass derivedClassUp2 = new DerivedClass();
+            Console.WriteLine(derivedClassUp2.GetHashCode());       // 12289376
+        }
+    }
+}
