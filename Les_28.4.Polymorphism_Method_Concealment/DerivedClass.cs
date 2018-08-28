6da1cceb56a8a861aeb90cb4ea4dcfc0a2e78930
Allega <42183540+MrsMars@users.noreply.github.com>
2018-08-28 21:10:32 +0300

    method concealment and 'new'

---
+++ Les_28.4.Polymorphism_Method_Concealment/DerivedClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_28
+{
+    class DerivedClass : BaseClass
+    {
+        // Чтобы подтвердить, что мы действительно хотим заменить метод из базового класса методом из произвольного, нужно написать 'new'
+        new public void Method()
+        {
+            Console.WriteLine("Method from derived class");
+        }
+    }
+}
---
+++ Les_28.4.Polymorphism_Method_Concealment/Program_28.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_28._4
+{
+    class Program_28
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.Method();                              // Method from derived class
+
+            BaseClass derivedClassUp = derivedClass;
+            derivedClassUp.Method();                            // Method from base class - открылся метод из базового класса тк привели производный класс к базовому
+
+            DerivedClass derivedClassDown = (DerivedClass)derivedClassUp;
+            derivedClassDown.Method();                          // Method from derived class - тк мы привели базовый класс к производому
+        }
+    }
+}
