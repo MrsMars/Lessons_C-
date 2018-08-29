151013ba21fd14a4b15dba73cfb290c06434b5a7
Allega <42183540+MrsMars@users.noreply.github.com>
2018-08-29 11:57:16 +0300

    Virtual methods and 'base'

---
+++ Les_30.Virtual_Methods_part_3/BaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_30
+{
+    class BaseClass
+    {
+        public virtual void VMethod()
+        {
+            Console.WriteLine("VMethod ftom base class");
+        }
+    }
+}
---
+++ Les_30.Virtual_Methods_part_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Вирутальные методы и к.с. base
+
+namespace Les_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+
+            derivedClass.VMethod();         // ReinitMethod from deriveedClass
+                                            // VMethod ftom base class
+        }
+    }
+}
