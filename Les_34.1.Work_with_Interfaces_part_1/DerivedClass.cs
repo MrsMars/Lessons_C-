6f4fb5a16636040252a3fd88bee4e6cf9ef6a386
Allega <42183540+MrsMars@users.noreply.github.com>
2018-08-31 14:41:37 +0300

    using interface part 1

---
+++ Les_34.1.Work_with_Interfaces_part_1/DerivedClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_34
+{
+    class DerivedClass : IInterfaceA, IInterfaceB
+    {
+        void IInterfaceA.Method()
+        {
+            Console.WriteLine("Method of interface A");
+        }
+        void IInterfaceB.Method()
+        {
+            Console.WriteLine("Method of interface B");
+        }
+    }
+}
