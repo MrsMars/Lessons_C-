5a1c398e1f2c4e1b96f273591ce9f6b36cb4926a
Allega <42183540+MrsMars@users.noreply.github.com>
2018-08-31 14:38:46 +0300

    interface part 1

---
+++ Les_33.1.Interface_part_1/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_33
+{
+    class DerivedClass : IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method from Derived class");
+        }
+    }
+}
