362325072784d2dc2ae15ccd4a27c006225d4a07
Allega <42183540+MrsMars@users.noreply.github.com>
2018-08-31 14:42:47 +0300

    using interfaces part 3

---
+++ Les_34.3.Work_with_Interfaces_part_3/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_34
+{
+    class DerivedClass : IInterfaceA, IInterfaceB
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method from interface A and B");
+        }
+    }
+}
---
+++ Les_34.3.Work_with_Interfaces_part_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_34._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+
+            derivedClass.Method();
+
+            IInterfaceA derivedClassUpToA = derivedClass as IInterfaceA;
+            derivedClassUpToA.Method();
+
+            IInterfaceB derivedClassUpToB = derivedClass;
+            derivedClassUpToB.Method();
+        }
+    }
+}
