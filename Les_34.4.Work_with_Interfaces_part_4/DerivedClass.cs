4a762fc078376f94cc39c1424afdf620d5c9cc83
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-05 12:43:57 +0300

    add work with interface part 4

---
+++ Les_34.4.Work_with_Interfaces_part_4/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_34
+{
+    class DerivedClass : AClass
+    {
+        public override int Addition(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
