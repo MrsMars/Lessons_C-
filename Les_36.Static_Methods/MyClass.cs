de560f31e9beb146ff99821b77f1f45de11d12fd
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-11 20:42:42 +0300

    static methods

---
+++ Les_36.Static_Methods/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_36
+{
+    class MyClass
+    {
+        public int age;
+        public int height;
+        public static int field;
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine(field);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Age - {age}\nHeight - {height}");
+        }
+    }
+}
---
+++ Les_36.Static_Methods/Program_36.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//  Статика
+
+namespace Les_36
+{
+    class Program_36
+    {
+        static void Main(string[] args)
+        {
+            MyClass mC1 = new MyClass();
+
+            mC1.age = 17;
+            mC1.height = 180;
+            mC1.GetInfo();
+
+            MyClass.field = 67;
+            MyClass.StaticMethod();
+
+            MyClass.field = 7;
+            Console.WriteLine(MyClass.field);
+        }
+    }
+}
