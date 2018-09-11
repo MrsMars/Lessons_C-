0c160f7e33d19fd493548ed19b1de0c234dd575c
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-11 20:46:59 +0300

    static classes

---
+++ Les_38.Static_Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Статический класс - контейнер, содержащий в себе только статические члены
+
+namespace Les_38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StaticClass.GetSum();       // 13.5
+
+            StaticClass.field = 30;
+            StaticClass.dField = 1.5;
+
+            StaticClass.GetSum();       // 31.5
+        }
+    }
+}
---
+++ Les_38.Static_Classes/StaticClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_38
+{
+    static class StaticClass
+    {
+        public static int field;
+        public static double dField;
+
+        public static void GetSum()
+        {
+            Console.WriteLine(field + dField);
+        }
+
+        static StaticClass()
+        {
+            field = 10;
+            dField = 3.5;
+        }
+    }
+}
