26671030ab7be5b743be68c0dee104571b37a152
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-11 20:44:58 +0300

    fabric method

---
+++ Les_37.2.Fabric_Methods/AbstractClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_37
+{
+    abstract class AbstractClass
+    {
+        public static AbstractClass Create()
+        {
+            return new MyClass();
+        }
+
+        public virtual void Method()
+        {
+
+        }
+    }
+}
---
+++ Les_37.2.Fabric_Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Фабричный метод возвращает экземпляр класса
+
+namespace Les_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass myClass = AbstractClass.Create();
+            // создали экземпляр класса и привели его к базовому типу
+
+            myClass.Method();
+        }
+    }
+}
