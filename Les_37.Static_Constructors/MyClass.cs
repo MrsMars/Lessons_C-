6fabcb265b4acd354b03ada6234eb7452aaf627e
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-11 20:43:41 +0300

    static constructors

---
+++ Les_37.Static_Constructors/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_37
+{
+    class MyClass
+    {
+        public static int field = 3;
+
+        static MyClass()
+        {
+            Console.WriteLine("Static constructor");
+        }
+        public MyClass()
+        {
+            Console.WriteLine("Usual constructor");
+        }
+    }
+}
---
+++ Les_37.Static_Constructors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Статический конструктор
+// не имеют модификаторов доступа и не имеют параметров
+// вызывается автоматически для инициализации класса - создаю экземпляр класса или получаю ссылку на статический член
+
+    
+
+namespace Les_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyClass mC1 = new MyClass();
+
+
+
+            MyClass.field = 6;
+        }
+    }
+}
