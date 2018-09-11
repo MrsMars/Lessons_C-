098db077509937ba1e08ffa194cab31d2ec3fcb3
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-11 20:45:55 +0300

    replace static methods

---
+++ Les_37.3.Static_Method_Replacing/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_37
+{
+    class DerivedClass : BaseClass
+    {
+        new public static void Method()
+        {
+            Console.WriteLine("Method from derived class");
+        }
+    }
+}
---
+++ Les_37.3.Static_Method_Replacing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Замещение статических методов
+
+namespace Les_37._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass.Method();
+            DerivedClass.Method();
+        }
+    }
+}
