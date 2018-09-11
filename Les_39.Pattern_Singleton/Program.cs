081f79addf3c55ff0e0df36fe4d6cef2e2f24bfb
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-11 20:48:13 +0300

    add pattern Singleton

---
+++ Les_39.Pattern_Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Патерн проектирования Singleton 
+
+// Singleton - это класс, который должен быть только один в системе или его должно быть только определённое количество экземпляров
+
+namespace Les_39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton s1 = new Singleton();
+
+            Singleton s1 = Singleton.Initialize();
+            Console.WriteLine(s1.GetHashCode());        // 46104728
+
+            Singleton s2 = Singleton.Initialize();
+            Console.WriteLine(s2.GetHashCode());        // 46104728
+        }
+    }
+}
---
+++ Les_39.Pattern_Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_39
+{
+    class Singleton
+    {
+        // приватное поле класса типа Singleton
+        private static Singleton single = null;
+
+        // защищённый конструтор
+        protected Singleton()
+        {
+
+        }
+        // Фабричный метод
+        public static Singleton Initialize()
+        {
+            if (single == null)
+                single = new Singleton();
+
+            return single;
+        }
+    }
+}
