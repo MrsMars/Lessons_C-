6fc07b091eb8923beec0953bab86a5c937e84681
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-12 11:08:46 +0300

    wid methods

---
+++ Les_40.Widening_Methods/ExtensionClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_40
+{
+    static class ExtensionClass
+    {
+        // этим методом мы расширим тип string
+        public static void SayHello(this string s)
+        {
+            Console.WriteLine($"{s}, Hello");
+        }
+
+        public static string AddStrings(this string s, params string[] strings)
+        {
+            for(int i = 0; i < strings.Length; i++)
+            {
+                s += $" {strings[i]}";
+            }
+            return s;
+        }
+    }
+}
---
+++ Les_40.Widening_Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Расширяющие методы AND Делегирование(не делегаты)
+
+// Расширяющие методы - позволяют добавлять методы в существующие типы без создания нового типа
+// Делегирование - передача собственности
+
+namespace Les_40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p = "JS";
+            p.SayHello();           // JS, Hello
+
+            ExtensionClass.SayHello("Python");      // Python, Hello
+
+            string str1 = "we love";
+            Console.WriteLine(str1.AddStrings("C#", "C++", "JS", "CSS3", "HTML5"));      // we love C# C++ JS CSS3 HTML5
+
+            string str2 = "I love".AddStrings("C#");
+            Console.WriteLine(str2);                                // I love C#
+        }
+    }
+}
