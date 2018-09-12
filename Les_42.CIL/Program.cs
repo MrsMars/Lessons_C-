48d74f95ba3f980c482d4d32359de98605854483
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-12 11:11:40 +0300

    CIL

---
+++ Les_42.CIL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// CIL. Основные понятия
+// чтобы посмотреть файл в cil-виде, нужно открыть командную строку разработчика в пуск / папка ВС и ввести ildasm
+// в проге отурыть exe файл
+
+namespace Les_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            int b = 10;
+
+            Console.WriteLine(a + b);
+            Console.ReadKey();
+        }
+    }
+}
