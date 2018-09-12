0cc1f79c36f091c527bdc29f2ad14ecf329a708e
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-12 11:12:36 +0300

    CIL Part 2

---
+++ Les_43.CIL.Part_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Les_43.TextFile1;
+using System.Reflection;
+
+// CIL. будем реализовывать статический метод в интерфейсе с помощью cil
+
+namespace Les_43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMagic.GetMagic();
+
+            Type type = typeof(IMagic);
+
+            MethodInfo method = type.GetMethod(nameof(IMagic.GetMagic));
+
+            method.Invoke(null, null);
+        }
+    }
+}
---
+++ Les_43.CIL.Part_2/TextFile2.bat
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework\v2.0.50727\ilasm.exe /dll D:\Work\C#\Byte++\Lesson_01\Les_43\TextFile1.il
+
+pause
