6963072b2f3c20932cc68251e1d42c536e37feb4
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-13 13:16:47 +0300

    add boxing and unboxing

---
+++ Les_46.Boxing_and_Unboxing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Boxing // Unboxing. Упаковка - преобразование структурного типа в ссылочный тип
+
+namespace Les_46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            // boxing   -   создали переменную типа object и записали в неё ссылку на объект, в котором содержится значение переменно i 
+            object obj = i;
+
+            // unboxing
+            int j = (int)obj;
+
+            Console.WriteLine(j);       // 10
+        }
+    }
+}
