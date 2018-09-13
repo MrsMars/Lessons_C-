cbe152dde71df14bf99949bef33d91d670906d13
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-13 13:22:23 +0300

    delegate

---
+++ Les_49.Delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// delegate - специальный объект, хранящий в себе указатель на метод (указатель на первый адрес байта, с которого начинается метод в памяти)
+
+namespace Les_49
+{
+    public delegate void MyDelegate();      // делегат
+
+    class SomeClass
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate mD = new MyDelegate(SomeClass.Method);       // создаём экземпляр делегата и передаём в конструктор класса делегата метод Method из SomeClass
+
+            mD.Invoke();        // or 'mD()'     запускает выполнение сообщённого метода
+        }
+    }
+}
