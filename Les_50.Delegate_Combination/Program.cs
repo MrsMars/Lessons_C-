71ee95b5f31d597f24f84f76b0ab8f79acd8503f
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-13 13:25:08 +0300

    comb del

---
+++ Les_50.Delegate_Combination/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Комбинированные делегаты
+
+namespace Les_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
