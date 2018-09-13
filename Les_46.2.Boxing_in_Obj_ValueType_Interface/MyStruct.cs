039726e63ea55278154324e5d5ac6a7e7145f448
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-13 13:19:07 +0300

    boxing in object, in ValueType and in interface

---
+++ Les_46.2.Boxing_in_Obj_ValueType_Interface/MyStruct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_46
+{
+    struct MyStruct : IInterface
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void SayGoodbay()
+        {
+            Console.WriteLine("Goodbay");
+        }
+    }
+}
---
+++ Les_46.2.Boxing_in_Obj_ValueType_Interface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStruct mS = new MyStruct();
+
+            // 1й тип упаковки (в object)
+            object boxed1 = mS;
+
+            // unboxing
+            MyStruct unboxed1 = (MyStruct)boxed1;
+            unboxed1.SayHello();                    // Hello
+
+
+            // 2й тип упаковки (в ValueType)
+            ValueType boxed2 = mS;
+
+            // unboxing
+            MyStruct unboxed2 = (MyStruct)boxed2;
+            unboxed2.SayHello();                    // Hello
+
+
+            // 3й тип упаковки (в interface)
+            IInterface boxed3 = mS;
+
+            // unboxing
+            MyStruct unboxed3 = (MyStruct)boxed3;
+            unboxed3.SayHello();                    // Hello
+            unboxed3.SayGoodbay();                  // Goodbay
+        }
+    }
+}
