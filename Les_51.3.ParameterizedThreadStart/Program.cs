625b5d7337a7ac950c8c198a1da730e26e0fbe2d
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-14 20:18:51 +0300

    add ParameterizedThreadStart
    part 3

---
+++ Les_51.3.ParameterizedThreadStart/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// ParameterizedThreadStart
+
+namespace Les_51._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 4;
+
+            // new thread
+            Thread myThread = new Thread(new ParameterizedThreadStart(Count));
+            myThread.Start(number);
+
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Main thread: {0}", (i * i));
+                Thread.Sleep(300);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Count(object x)
+        {
+            for(int i = 1; i < 9; i++)
+            {
+                int n = (int)x;
+
+                Console.WriteLine("Second thread: {0}", (i * n));
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
