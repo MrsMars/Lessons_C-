52d3d0f721224b1e6ab87cd2e8cab5fd1a2d316e
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-15 22:07:51 +0300

    arrayList part 2

---
+++ Les_53.2.Collection_2.ArrayList.Part_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ArrayList
+
+namespace Les_53._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            list.Add(2.3);      // add double value
+            list.Add(55);       // add int
+            list.AddRange(new string[] { "Hello", "world" });   // add string array
+
+            foreach (object o in list) { Console.WriteLine(o); }
+
+            list.RemoveAt(0);   // del first el
+            list.Reverse();     // reverse array
+
+            Console.WriteLine(list[0]);     // world
+
+            for (int i = 0; i < list.Count; i++) { Console.WriteLine(list[i]); }
+
+            Console.ReadLine();
+        }
+    }
+}
