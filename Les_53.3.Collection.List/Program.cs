8ab0c55c7c882b8364bdb2e64fd8b4d5c6951805
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-15 22:12:17 +0300

    add new collection 'List'

---
+++ Les_53.3.Collection.List/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_53._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int 
+            List<int> numbers = new List<int>() { 1, 2, 3, 45 };
+
+            numbers.Add(6);
+            numbers.AddRange(new int[] { 7, 8, 9 });
+            numbers.Insert(0, 777);     // add 777 on the first place
+            numbers.RemoveAt(1);        // del 2nd el
+
+            foreach (int i in numbers) { Console.WriteLine(i); }
+
+            // class
+            List<Person> people = new List<Person>(3);      // 3 - size (capacity)
+
+            Console.WriteLine(people.Capacity);
+            people.Capacity = 4;
+            Console.WriteLine(people.Capacity);
+
+            people.Add(new Person() { Name = "Tom" });
+            people.Add(new Person() { Name = "John" });
+        }
+
+        class Person
+        {
+            public string Name { get; set; }
+        }
+    }
+}
