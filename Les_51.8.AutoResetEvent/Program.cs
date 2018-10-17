496c4bd23014df9d1f803dc269cb59d38f061cdd
Allega <42183540+MrsMars@users.noreply.github.com>
2018-10-17 11:59:55 +0300

    auto reset event

---
+++ Les_51.8.AutoResetEvent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// AutoResetEvent
+
+namespace Les_51._8
+{
+    class Program
+    {
+        static AutoResetEvent waitHandler = new AutoResetEvent(true);
+        static void Main(string[] args)
+        {
+        }
+    }
+}
