8f5188f4d00b823eb86be882c2dd585cca00da62
Allega <42183540+MrsMars@users.noreply.github.com>
2018-10-19 10:46:14 +0300

    read and write data in file

---
+++ Les_54.5.IO.Read_&_Write/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_54._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read
+            string path = @"D:\Work\SomeStr_1.txt";
+
+            try
+            {
+                Console.WriteLine("*** *** *** read all file *** *** ***");
+                using (StreamReader sr = new StreamReader(path)) { Console.WriteLine(sr.ReadToEnd()); }
+
+                Console.WriteLine("\n*** *** *** read every string *** *** ***");
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null) { Console.WriteLine(line); }
+                }
+
+                Console.WriteLine("\n*** *** *** read every block *** *** ***");
+                using(StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    char[] array = new char[4];
+                    sr.Read(array, 0, 4);
+                    Console.WriteLine(array);
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            // write
+            string readPath = @"D:\Work\SomeStr_1.txt";
+            string writePath = @"D:\Work\SomeStr_2.txt";
+            string text = "";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(readPath, System.Text.Encoding.Default)) { text = sr.ReadToEnd(); }
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default)) { sw.WriteLine(text); }
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default)) { sw.WriteLine("Add", " Hello"); }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
